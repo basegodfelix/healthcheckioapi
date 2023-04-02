16e40289087f4e1212dedbc6dcda75787740f493
BaseGodFelix <felixmobile87@gmail.com>
2023-04-02 09:48:17 -0400

    fixing text displays

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 import setuptools
+import os
+
+try:
+    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
 
 setuptools.setup(
     name="HealthCheckIOAPI",
@@ -6,5 +13,9 @@ setuptools.setup(
     author="Felix Hernandez",
     description="Simple Python Tooling to interact with Healthcheck.io projects.",
     packages=["healthcheckio"],
-    install_requires=["requests"]
+    install_requires=["requests"],
+    url="https://github.com/basegodfelix/healthcheckioapi"
+    long_description = long_description,
+    long_description_context_type = 'text/markdown',
+    license="MIT"
 )
