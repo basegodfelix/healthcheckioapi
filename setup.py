5f640f30e11e04febc137f285a2d7a63f6689f6f
BaseGodFelix <felixmobile87@gmail.com>
2023-04-10 09:53:43 -0400

    fixing setup

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 import setuptools
 import os
 
-try:
-    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
-except Exception:
-    long_description = ''
+def read():
+    tmp = ""
+    path = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(path, 'README.md'), encoding='utf-8') as f:
+        tmp = f.read()
+    return tmp
 
 setuptools.setup(
     name="HealthCheckIOAPI",
-    version="1.0.1",
+    version="1.0.2",
     author="Felix Hernandez",
     description="Simple Python Tooling to interact with Healthcheck.io projects.",
     packages=["healthcheckio"],
     install_requires=["requests"],
     url="https://github.com/basegodfelix/healthcheckioapi",
-    long_description = long_description,
+    long_description = read(),
     long_description_context_type = 'text/markdown',
     license="MIT"
 )
